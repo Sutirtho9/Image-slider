0f438b4bfb7365229fa2db5081008841906a2b1f
Sutirtho9 <sutirthochakravorty@gmail.com>
2025-06-30 18:10:34 +0530

    dot functionality added

--- script.js
+++ script.js
@@ -5,6 +5,37 @@ let currentIndex = 1;
 const slider = document.querySelector('.slider');
 
 
+const bottom = document.querySelector('.bottom');
+
+for(let i=0;i<images.length;i++){
+    const div = document.createElement('div');
+    div.className ='button';
+    bottom.appendChild(div);
+}
+const buttons = document.querySelectorAll('.button');
+
+buttons[0].style.backgroundColor='white';
+
+const resetBg = ()=>{
+    buttons.forEach((button)=>{
+        button.style.backgroundColor='transparent';
+    })
+}
+
+buttons.forEach((button,i) =>{
+    button.addEventListener('click',()=>{
+        slider.style.transform =`translate(-${i*700}px)`;
+        resetBg();
+        currentIndex=i+1;
+        button.style.backgroundColor='white';
+
+    })
+})
+
+const changeColor = () =>{
+    resetBg();
+    buttons[currentIndex-1].style.backgroundColor ='white';
+}
 
 function nextImage() {
     if (currentIndex >= images.length) {
@@ -24,8 +55,13 @@ function prevImage(){
 
 right.addEventListener('click',()=>{
     nextImage();
+    changeColor();
 })
 
 left.addEventListener('click',()=>{
     prevImage();
+    changeColor();
 })
+
+
+
